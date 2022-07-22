4058d01f8eb8c93a13470b0c14673c607f3f1005
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-07-22 08:55:40 -0300

    feat: api gatteway zuul config

---
+++ hr-api-gateway-zuul/src/test/java/com/domingueti/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingueti.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
