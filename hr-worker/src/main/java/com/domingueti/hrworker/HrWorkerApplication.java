4d05c48d7d796d1b215e266604a5f3f9826cc7bf
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-08-01 05:39:59 -0300

    hr-user

--- hr-worker/src/main/java/com/domingueti/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/domingueti/hrworker/HrWorkerApplication.java
@@ -1,15 +1,25 @@
 package com.domingueti.hrworker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrWorkerApplication {
+public class HrWorkerApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String value;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(value);
 	}
 
 }
