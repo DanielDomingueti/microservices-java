a8e988250879b6417640916af95fb50c0b5148fa
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-08-01 15:52:03 -0300

    feat: FeignClient

---
+++ hr-oauth/src/main/java/com/domingueti/hroauth/entities/User.java
@@ -0,0 +1,24 @@
+package com.domingueti.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private Set<Role> roles = new HashSet<>();
+}
